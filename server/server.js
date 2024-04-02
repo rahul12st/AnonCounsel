4b47ec24e3526c9b6e4e42f08058b761831abb7d
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-04-02 21:37:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 const app = express();
 app.use(cors(
   {
-    origin: ["https://linea-gpt.vercel.app/"],
+    origin: "https://linea-gpt.vercel.app/",
     methods: ["POST","GET"],
     credentials: true
   }
@@ -37,7 +37,9 @@ app.post("/chat", async (req, res) => {
       frequency_penalty: 0.5,
       presence_penalty: 0,
     });
-
+ res.setHeader("Access-Control-Allow-Origin", "https://linea-gpt.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.status(200).send({
       bot: response.choices[0].text,
     });
