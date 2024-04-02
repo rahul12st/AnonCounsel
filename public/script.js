e34451d3a625b5cae7c3d31bf5253961672538a9
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-04-02 19:14:03 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -80,7 +80,7 @@ export const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://linea-gpt.vercel-api.app/chat", {
+  const response = await fetch("https://linea-gpt.vercel-api.app/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
