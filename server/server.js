17174c13e639e0f34454afb554862868142a3dc8
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-04-05 01:47:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -34,10 +34,7 @@ app.post("/chat", async (req, res) => {
       frequency_penalty: 0.5,
       presence_penalty: 0,
     });
- // res.setHeader("Access-Control-Allow-Origin", "https://linea-gpt.vercel.app");
-     
- //    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
- //    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     res.status(200).send({
       bot: response.choices[0].text,
     });
