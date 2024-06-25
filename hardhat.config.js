1205e705fea5856766cf6c5cbdd740474eb92275
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-06-25 15:59:12 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     hardhat: {},
-    polygon_mumbai:{
-      url:"https://polygon-mumbai.g.alchemy.com/v2/EM2AHR0wZICPaLznaNDReMWZWF9T1cuf",
+    sepolia:{
+      url:"https://linea-sepolia.infura.io/v3/2XfGWfBKHPBW1uVGHs6SOWjYfI1",
       accounts:[
       `0x${"267861b4cee6ca9bdf99cdc35e158ebdfbb6d3aaad85720e0627784f367aa656"}`,
       ],
